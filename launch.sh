4e60141004a72c3124ae813ecdc69d3f11af3348
ttallman <dspiteself@gmail.com>
2016-11-07 21:43:52 -0600

    tail starts with 1 not 0

--- launch.sh
+++ launch.sh
@@ -6,7 +6,7 @@ if [ -a $OFFSET_FILE ]
     then
     N=$(<"$OFFSET_FILE")
    else
-      `echo "0" > $OFFSET_FILE`;
+      `echo "1" > $OFFSET_FILE`;
 fi
 echo $N  "$OFFSET_FILE"
 tail -f -n +$N "$FILENAME" | { ./.stack-work/install/x86_64-linux/lts-7.8/8.0.1/bin/lship-exe --offset-out $OFFSET_FILE; pkill -P$$ tail; }
